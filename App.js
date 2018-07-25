1569de9bc1fcb8c5d5a90bc3f79eb64ecd358f45
Sergio Junca <sergio.junca@gmail.com>
2018-07-25 18:41:10 -0500

    Updated read-me

--- App.js
+++ App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
 
-import { Button } from 'antd-mobile-rn';
+import { ScrollView, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Button, Flex, WhiteSpace, WingBlank } from 'antd-mobile-rn';
+
+import { TabBar } from 'antd-mobile-rn';
+
+
+const TabBarItem = TabBar.Item;
+const FlexItem = Flex.Item;
+
 
 export default class App extends React.Component {
   render() {
+    
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Button>antd-mobile-rn button</Button>
-      </View>
+      <WingBlank style={{ marginTop: 10 }}>
+          <Flex>
+            <FlexItem style={{ paddingLeft: 10, paddingRight: 10 }}>
+              <Button>FlexItem #1</Button>
+            </FlexItem>
+            <FlexItem style={{ paddingLeft: 10, paddingRight: 10 }}>
+              <Button>FlexItem #2</Button>
+            </FlexItem>
+            <FlexItem style={{ paddingLeft: 10, paddingRight: 10 }}>
+              <Button>FlexItem #3</Button>
+            </FlexItem>
+            <FlexItem style={{ paddingLeft: 10, paddingRight: 10 }}>
+              <Button>FlexItem #4</Button>
+            </FlexItem>
+          </Flex>
+        </WingBlank>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     display: "flex",
+//     width: "100%",
+//     height: "100%",
+//     flexDirection: "row",
+//     flexWrap: "nowrap",
+//     backgroundColor: '#cfd8dc',
+//     alignItems: "flex-start",
+//     justifyContent: "flex-start"
+//   },
+// });
+
--- README.md
+++ README.md
@@ -7,7 +7,15 @@ Installation steps:
 2. npm i
 3. npm run android
 
-#Troubleshoot
+Install dependecies:
+1. $ npm install antd-mobile-rn --save
+2. $ npm install babel-plugin-import --save-dev
+3. $ npm install babel-preset-expo --save
+
+Config for android.Gradle:
+In your local.properties, you want to set the variable sdk.dir to the path of your SDK folder:
+sdk.dir=/home/username/Android/Sdk
+# Troubleshoot
 
 [Refer to the user installation guide](https://docs.google.com/document/d/1cwEN5sbAiRQi28vq63DrsLmD2Hsf1X4lqdTHSquO0N4/edit?usp=sharing)
 
@@ -15,3 +23,4 @@ Installation steps:
 ## Images
 First image after installing the SDK and running the project 
 ![Screen Image](https://i.imgur.com/61uaI4h.png)
+
